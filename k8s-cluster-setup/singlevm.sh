23026827c421152132e6192823b33f0bd389ca7e
nn-advith <advith.nn@gmail.com>
2025-10-05 21:21:46 +0530

    preexec validation; sample spec.json

--- k8s-cluster-setup/singlevm.sh
+++ k8s-cluster-setup/singlevm.sh
@@ -1,30 +1,106 @@
 #!/bin/bash
 
-
-
 VMNAME="ubuntu-vm"
 ISOPATH="$HOME/iso/ubuntu-25.04-live-server-amd64.iso"
 SEEDISOPATH="$PWD/seed.iso"
 VDI="$HOME/VirtualBox VMs/${VMNAME}/${VMNAME}.vdi"
-# preinstall steps that need to be done on hopst machine:
-# sudo apt-get install virtualbox-guest-additions-iso  -- for --install-additions to work
-# sudo apt-get install cloud-utils -- for seed.iso and user-data
-# (OR equivalent in other os)
 
-cp "$PWD/user-data.yaml" "$PWD/user-data"
-cloud-localds seed.iso "$PWD/user-data"
+SPECJSONPATH=""
 
-function unmount
 
-alias vbm="VBoxManage"
+function usage() {
+    # sh singlevm.sh -i spec.json
+    echo -e "\nUsage:"
+    echo -e "\tbash singlevm.sh --input|-i <SPEC JSON file path> [--help|-h]"
+}
 
-vbm createvm --name "${VMNAME}" --register
-vbm modifyvm "${VMNAME}" --memory 4096 --cpus 2 --ostype Ubuntu_64 --nic1 nat
-vbm modifyvm "${VMNAME}" --nic2 hostonly --hostonlyadapter2 vboxnet0 # probably customise this
-vbm createmedium disk --filename "${VDI}" --size 20000
-vbm storagectl "${VMNAME}" --name "SATA-C0" --add sata --controller IntelAHCI
-vbm storageattach "${VMNAME}" --storagectl "SATA-C0" --port 0 --device 0 --type hdd --medium "${VDI}"
-vbm storageattach "${VMNAME}" --storagectl "SATA-C0" --port 1 --device 0 --type dvddrive --medium "${ISOPATH}"
-vbm storageattach "${VMNAME}" --storagectl "SATA-C0" --port 2 --device 0 --type dvddrive --medium "${SEEDISOPATH}"
+function validateMangatoryFlags() {
+    if [[ -z $SPECJSONPATH ]];then
+        echo -e "ERROR: --input is mandatory"
+        usage
+        exit 1
+    fi
+    if [[ ! -f "$SPECJSONPATH" ]];then
+        echo -e "ERROR: input json [$SPECJSONPATH] not found"
+        exit 1
+    fi
+}
 
-vbm startvm "${VMNAME}" --type=headless
+function preExecCheck() {
+    # check for guest-additions package and cloud-localds
+    # asssumes Ubuntu HOST machine
+    # can be modified to get type of HOST OS and suggest installation commands
+    missing=()
+    if ! which VBoxManage &> /dev/null;then
+        missing+=("VBoxManage;sudo apt-get install virtualbox")
+    fi
+    if ! which cloud-localds &> /dev/null;then
+        missing+=("cloud-localds;sudo apt-get install cloud-utils")
+    fi
+    if ! dpkg -l | grep '^ii' | grep -q virtualbox-guest-additions-iso; then
+        missing+=("virtualbox-guest-additions-iso;sudo apt-get install virtualbox-guest-additions-iso")
+    fi
+    if [[ ${#missing[@]} -gt 0 ]];then 
+        echo -e "ERROR: Required pacakges not found; please install to proceed"
+        for i in "${missing[@]}";do
+            echo -e "  - $(echo $i | cut -d ';' -f 1)\n    INSTALL: $(echo $i | cut -d ';' -f 2)"
+        done
+        echo -e "\033[031mAborting\033[0m"
+        exit 1
+    fi
+}
+
+function preExecInputValidation() {
+    # check if inputs are valid
+    if ! jq empty $SPECJSONPATH 2>/dev/null;then
+        echo -e "ERROR: JSON validation failed for $SPECJSONPATH \n\033[031mAborting\033[0m"
+        exit 1
+    fi
+}
+
+# function installSingleVM() {
+
+# }
+
+# function unmountiso() {
+
+# }
+
+while [[ $# -gt 0 ]];do
+    case "$1" in
+        -i|--input)
+            SPECJSONPATH="$2"
+            shift 2
+        ;;
+        -h|--help)
+            usage
+            exit 0
+        ;;
+        *)
+            echo "unknown flag $1"
+            usage
+            exit 1
+        ;;
+    esac
+done
+
+validateMangatoryFlags
+preExecInputValidation
+preExecCheck
+
+# cp "$PWD/user-data.yaml" "$PWD/user-data"
+# cloud-localds seed.iso "$PWD/user-data"
+
+
+# alias vbm="VBoxManage"
+
+# vbm createvm --name "${VMNAME}" --register
+# vbm modifyvm "${VMNAME}" --memory 4096 --cpus 2 --ostype Ubuntu_64 --nic1 nat
+# vbm modifyvm "${VMNAME}" --nic2 hostonly --hostonlyadapter2 vboxnet0 # probably customise this
+# vbm createmedium disk --filename "${VDI}" --size 20000
+# vbm storagectl "${VMNAME}" --name "SATA-C0" --add sata --controller IntelAHCI
+# vbm storageattach "${VMNAME}" --storagectl "SATA-C0" --port 0 --device 0 --type hdd --medium "${VDI}"
+# vbm storageattach "${VMNAME}" --storagectl "SATA-C0" --port 1 --device 0 --type dvddrive --medium "${ISOPATH}"
+# vbm storageattach "${VMNAME}" --storagectl "SATA-C0" --port 2 --device 0 --type dvddrive --medium "${SEEDISOPATH}"
+
+# vbm startvm "${VMNAME}" --type=headless
